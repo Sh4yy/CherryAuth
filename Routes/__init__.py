4f34bf3f6b3352ceec5e6c1da180084a89508f86
sh4yy <shayan@umd.edu>
2019-07-03 15:43:33 -0400

    change password route

--- Routes/__init__.py
+++ Routes/__init__.py
@@ -18,7 +18,6 @@ def err_resp(code: int, msg: str):
     :param msg: error msg
     :return: JsonResponse
     """
-
     return JsonResponse({
         "ok": False,
         "code": code,
@@ -36,6 +35,19 @@ def suc_resp(content):
     content['ok'] = True
     content['timestamp'] = time()
     return JsonResponse(content)
+
+
+def extract_basic(authorization):
+    """
+    extract basic authorization from header
+    :param authorization: Authorization header value
+    :return: gid, password on success
+    :raises TokenDoesNotExist:
+    """
+    if not authorization:
+        raise TokenDoesNotExist()
+
+    return basicauth.decode(authorization)
 
 
 def extract_bearer(authorization):
@@ -68,16 +80,19 @@ async def register(request: Request):
     provide user gid and password to register
     Authorization: Basic gid:password
     """
-    gid, pswd = basicauth.decode(request.headers.get('Authorization'))
     try:
+
+        gid, pswd = extract_basic(request.headers.get('Authorization'))
         user = Controllers.register(gid, pswd)
+        return suc_resp({
+            "gid": user.gid,
+            "reg_date": user.reg_date
+        })
+
+    except TokenDoesNotExist:
+        return err_resp(400, "missing authorization header")
     except UserAlreadyExist:
         return err_resp(400, "user already exist")
-
-    return suc_resp({
-        "gid": user.gid,
-        "reg_date": user.reg_date
-    })
 
 
 @bp.route('/login', methods=['GET'])
@@ -86,21 +101,24 @@ async def login(request: Request):
     login a user
     provide user gid and password as basic auth
     """
-    gid, pswd = basicauth.decode(request.headers.get('Authorization'))
     try:
+
+        gid, pswd = extract_basic(request.headers.get('Authorization'))
         session = Controllers.login(gid, pswd)
+        jwt_token, payload = session.generate_jwt()
+        return suc_resp({
+            "jwt": {
+                "token": jwt_token,
+                "refresh_token": session.refresh_token,
+                "payload": payload
+            },
+            "gid": gid
+        })
+
+    except TokenDoesNotExist:
+        return err_resp(400, "missing authorization header")
     except DoesNotExist:
         return err_resp(404, "user does not exist")
-
-    jwt_token, payload = session.generate_jwt()
-    return suc_resp({
-        "jwt": {
-            "token": jwt_token,
-            "refresh_token": session.refresh_token,
-            "payload": payload
-        },
-        "gid": gid
-    })
 
 
 @bp.route('/token/logout', methods=['POST'])
@@ -108,26 +126,24 @@ async def logout(request: Request):
     """
     provide refresh token as Bearer token
     """
-
     try:
+
         token = extract_bearer(request.headers.get("Authorization"))
+        session = Controllers.logout(token)
+        return suc_resp({
+            "logged_out": True,
+            "token": token,
+            "gid": session.user.gid
+        })
+
     except InvalidMethod:
         err_resp(400, "bad authorization method")
     except InvalidValue:
         err_resp(400, "bad authorization token")
     except TokenDoesNotExist:
         return err_resp(400, "missing authorization token")
-
-    try:
-        session = Controllers.logout(token)
     except DoesNotExist:
         return err_resp(404, "session was not found")
-
-    return suc_resp({
-        "logged_out": True,
-        "token": token,
-        "gid": session.user.gid
-    })
 
 
 @bp.route('/token/refresh', methods=['GET'])
@@ -136,53 +152,70 @@ async def refresh_token(request: Request):
     refresh jwt token using refresh token
     Authorization: Bearer [Refresh Token]
     """
-
     try:
+
         token = extract_bearer(request.headers.get("Authorization"))
-    except InvalidMethod:
-        err_resp(400, "bad authorization method")
-    except InvalidValue:
-        err_resp(400, "bad authorization token")
-    except TokenDoesNotExist:
-        return err_resp(400, "missing authorization token")
-
-    try:
         session = Controllers.refresh_token(token)
+        jwt_token, payload = session.generate_jwt()
+        return suc_resp({
+            "jwt": {
+                "token": jwt_token,
+                "refresh_token": session.refresh_token,
+                "payload": payload
+            },
+            "gid": session.user.gid
+        })
+
+    except InvalidMethod:
+        return err_resp(400, "bad authorization method")
+    except InvalidValue:
+        return err_resp(400, "bad authorization token")
+    except TokenDoesNotExist:
+        return err_resp(400, "missing authorization header")
     except DoesNotExist:
         return err_resp(404, "session was not found")
-
-    jwt_token, payload = session.generate_jwt()
-    return suc_resp({
-        "jwt": {
-            "token": jwt_token,
-            "refresh_token": session.refresh_token,
-            "payload": payload
-        },
-        "gid": session.user.gid
-    })
 
 
 @bp.route('/password/change', methods=['POST'])
 async def change_password(request: Request):
-    # provide old password and new
-
-    gid, old_pswd = basicauth.decode(request.headers.get('Authorization'))
-    json_data = await request.json()
-    if 'new_password' not in json_data:
-        return err_resp(400, "missing new_password field")
-
-    new_password = json_data['new_password']
-    terminate_sessions = json_data.get('terminate_sessions', False)
-
+    """
+    change user's password
+    requires old password as basic authorization
+    and new_password field in body as well as an
+    optional kill_sessions boolean field to kill
+    all the currently active sessions.
+    """
     try:
-        Controllers.change_password(gid, old_pswd, new_password, terminate_sessions)
-    except:
-        pass
+        gid, old_pswd = extract_basic(request.headers.get('Authorization'))
+        json_data = await request.json()
+
+        if 'new_password' not in json_data:
+            return err_resp(400, 'missing new_password field')
+        new_password = json_data.get('new_password')
+        kill_sessions = json_data.get('kill_sessions', False)
+        Controllers.change_password(gid, old_pswd, new_password, kill_sessions)
+        return suc_resp({
+            "changed_password": True,
+            "killed_sessions": kill_sessions,
+        })
+
+    except TokenDoesNotExist:
+        return err_resp(400, "missing authorization header")
+    except DoesNotExist:
+        return err_resp(404, "user was not found")
+    except WrongPassword:
+        return err_resp(401, "wrong password")
 
 
-@bp.route('/password/forgot', methods=['POST'])
-async def forgot_password(request: Request):
+@bp.route('/password/reset/request', methods=['POST'])
+async def reset_password_request(request: Request):
     # issue temp token for password reset
+    pass
+
+
+@bp.route('/password/reset', methods=['POST'])
+async def reset_password(request: Request):
+    # provide password token for reset
     pass
 
 
